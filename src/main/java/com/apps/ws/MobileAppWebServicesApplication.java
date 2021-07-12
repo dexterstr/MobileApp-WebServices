24feab72212ddc7a986de2bbaaf5d6492d24fb62
Tarun <tarun.sarpanjeri@gmail.com>
2021-07-11 22:45:07 -0400

    Updated pom xml for generating war file

--- src/main/java/com/apps/ws/MobileAppWebServicesApplication.java
+++ src/main/java/com/apps/ws/MobileAppWebServicesApplication.java
@@ -8,39 +8,39 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.apps.ws.security.AppProperties;
+import com.apps.ws.SpringApplicationContext;
 
 @SpringBootApplication
-public class MobileAppWebServicesApplication  extends SpringBootServletInitializer{
+public class MobileAppWebServicesApplication extends SpringBootServletInitializer {
 
-	//For generating Web Application Archive(WAR) we comment this code and override it
-	//WAR to deploy in standalone apache tomcat
-//	public static void main(String[] args) {
-//		SpringApplication.run(MobileAppWebServicesApplication.class, args);
-//	}
+	// For generating Web Application Archive(WAR) we comment this code and override
+	// it
+	// WAR to deploy in standalone apache tomcat
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAppWebServicesApplication.class, args);
+	}
 
 	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(MobileAppWebServicesApplication.class);
 	}
 
 	@Bean
-	public BCryptPasswordEncoder  bCryptPasswordEncoder(){
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Bean
-	public SpringApplicationContext springApplicationContext(){
-		
+	public SpringApplicationContext springApplicationContext() {
+
 		return new SpringApplicationContext();
 	}
-	
-	@Bean(name="AppProperties")
+
+	@Bean(name = "AppProperties")
 	public AppProperties getAppProperties() {
-		
+
 		return new AppProperties();
 	}
 
 }
-
-
- 
--- src/main/java/com/apps/ws/security/AppProperties.java
+++ src/main/java/com/apps/ws/security/AppProperties.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AppProperties {
+	
 	@Autowired
 	private Environment env;
 	
